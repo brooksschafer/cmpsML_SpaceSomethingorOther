8619b942b41150b38009e4670ba41f40bcea083c
brooks <brooks.schafer@selu.edu>
2024-04-24 01:18:47 -0500

    did epoch curves + performance measures + ppt

--- CODE/MODEL/PA4_SpaceSomethingorOther.py
+++ CODE/MODEL/PA4_SpaceSomethingorOther.py
@@ -1,5 +1,5 @@
 #%% MODULE BEGINS
-module_name = 'PA3'
+module_name = 'PA4'
 '''
 Version: <***>
 Description:
@@ -15,7 +15,9 @@ Notes:
 #%% IMPORTS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 if __name__ == "__main__":
     import os
-    os.chdir(r"C:\Users\brook\OneDrive\Documents\GitHub\cmpsML_SpaceSomethingorOther\CODE")
+    base_path = r"C:\Users\brook\OneDrive\Documents\GitHub\cmpsML_SpaceSomethingorOther\CODE"
+    path = os.path.join(base_path, r"C:\Users\melof\OneDrive\Documents\GitHub\cmpsML_SpaceSomethingorOther\CODE")
+    # os.chdir(r"C:\Users\brook\OneDrive\Documents\GitHub\cmpsML_SpaceSomethingorOther\CODE")
     # os.chdir(r"C:\Users\melof\OneDrive\Documents\GitHub\cmpsML_SpaceSomethingorOther\CODE")
 
 #custom imports
@@ -26,13 +28,17 @@ from matplotlib import pyplot as plt
 import scipy.signal as signal
 import numpy as np
 #import mne
+import csv
 import pandas as pd
 import seaborn as sns
 import pickle as pckl
 from scipy.stats import kurtosis, skew
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import roc_auc_score, accuracy_score, precision_score, recall_score, f1_score
 #
 #%% USER INTERFACE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-chLabel = input("Enter a channel label(ex, M1):")
+chLabel = input("Enter a channel label(ex, M1):").upper()
 #
 #%% CONSTANTS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #%% CONFIGURATION ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -42,7 +48,8 @@ chLabel = input("Enter a channel label(ex, M1):")
 #Class definitions Start Here
 #Function definitions Start Here
 def getIndex(chLabel):
-    pathSoIRoot = 'INPUT\\DataSmall\\sb1\\se1'
+    pathSoIRoot = os.path.join(path, 'INPUT','DataSmall','sb1','se1')
+    # pathSoIRoot = 'INPUT\\DataSmall\\sb1\\se1'
     pathSoi = f'{pathSoIRoot}\\'
     soi_file = '1_1_bk_pic.pckl'
     #Load SoI objectM1
@@ -125,12 +132,57 @@ def visualize(features, chLabel):
     plt.suptitle(chLabel + " Features\n", fontsize=18)
     for i, f1, f2 in zip(range(1,5), ['f0', 'f2', 'f4', 'f6'], ['f1', 'f3', 'f5', 'f7']):
         plt.subplot(2, 2, i)
-        plt.scatter(features[features['class'] == 0][f1], features[features['class'] == 0][f2], c='g', label='sb1(class 0)', alpha=0.6)
-        plt.scatter(features[features['class'] == 1][f1], features[features['class'] == 1][f2], c='b', label='sb2(class 1)', alpha=0.6)
+        unique_f1 = sorted(features[f1].unique())
+        plt.hist(features[features['class'] == 0][f1], bins=unique_f1, weights=features[features['class'] == 0][f2], color='g', label='sb1(class 0)', alpha=0.75)
+        plt.hist(features[features['class'] == 1][f1], bins=unique_f1, weights=features[features['class'] == 1][f2], color='b', label='sb2(class 1)', alpha=0.75)
         plt.title(f1 + ' vs ' + f2, fontsize=16, loc='left')
         plt.xlabel(f1, fontsize=15)
         plt.ylabel(f2, fontsize=15)
         plt.legend()
+    plt.tight_layout()
+    plt.show()
+    
+def three_tier_test(features, chLabel):
+    train_val, test = train_test_split(features, test_size=0.2, random_state=None)
+    train, val = train_test_split(train_val, test_size=.25, random_state=None)
+    
+    X_train, Y_train = train.drop(columns=['class']), train['class'].astype('int')
+    X_val, Y_val = val.drop(columns=['class']), val['class'].astype('int')
+    X_test, Y_test = test.drop(columns=['class']), test['class'].astype('int')
+    
+    model = MLPClassifier(hidden_layer_sizes=100, max_iter=500, activation='logistic').fit(X_train, Y_train)
+    plt.plot(model.loss_curve_, label='Training', c='b', alpha=0.75)
+    model.fit(X_val, Y_val)
+    plt.plot(model.loss_curve_, label='Validation', c='g', alpha=0.75)
+    plt.title(chLabel + ' Epoch Error Curve')
+    plt.xlabel('Epoch')
+    plt.ylabel("Error")
+    plt.legend()
+
+    val_predictions = model.predict(X_val)
+    test_predictions = model.predict(X_test)
+    
+    return {'ValTruth': Y_val, 'ValPrediction': val_predictions, 'TestTruth': Y_test, 'TestPrediction': test_predictions} 
+
+def plotPerformance(modelResults, chLabel):
+    #Calculating and plotting performance measures for val and test data
+    plt.figure(figsize=(10,8))
+    plt.title(chLabel + ' Performance Measures', fontsize=16)
+    barWidth = 0.3
+    for dataset in ['Val', 'Test']:
+        precision =   precision_score(modelResults[dataset+'Truth'], modelResults[dataset+'Prediction'])
+        recall =      recall_score(modelResults[dataset+'Truth'], modelResults[dataset+'Prediction'])
+        f1 =          f1_score(modelResults[dataset+'Truth'], modelResults[dataset+'Prediction'])
+        accuracy =    accuracy_score(modelResults[dataset+'Truth'], modelResults[dataset+'Prediction'])
+        specificity = recall_score(modelResults[dataset+'Truth'], modelResults[dataset+'Prediction'], pos_label=0)
+        auc =         roc_auc_score(modelResults[dataset+'Truth'], modelResults[dataset+'Prediction'])       
+        performanceMeasures = {'Precision': precision, 'Recall': recall, 'F1': f1, 'Accuracy': accuracy, 'Specificity': specificity, 'AUC': auc}
+        if dataset == 'Val':
+            plt.bar(performanceMeasures.keys(), performanceMeasures.values(), -barWidth, align='edge', color='g', label=dataset)
+        else:
+            plt.bar(performanceMeasures.keys(), performanceMeasures.values(), +barWidth, align='edge', color='b', label=dataset)  
+    #plt.xaxis.set_tick_params(labelsize='medium')
+    plt.legend()
     plt.tight_layout()
     plt.show()
 #
@@ -143,7 +195,8 @@ def main():
     streamID = 0
     for sb in ['sb1', 'sb2']:
         for se in ['se1', 'se2']:
-            pathSoIRoot = 'INPUT\\DataSmall\\' + sb + '\\' + se
+            pathSoIRoot = os.path.join(path, 'INPUT','DataSmall',sb,se)
+            # pathSoIRoot = 'INPUT\\DataSmall\\' + sb + '\\' + se
             files = os.listdir(pathSoIRoot)
             for file in files:
                 pathSoi = f'{pathSoIRoot}\\'
@@ -162,14 +215,24 @@ def main():
                 streamID += 1
     features.loc[features['sb'] == 'sb2', 'class'] = 1
     
+    #Trying out getting rid of sb se from columns
+    features = pd.get_dummies(features, columns=['sb','se'])
+    
     #Visualize features
     visualize(features, chLabel)
     #Split train/val and test data
-    trainVal = features.loc[features['se'] == 'se1']
-    test = features.loc[features['se'] == 'se2']
+    trainVal = features.loc[features['se_se1'] == 'se1']
+    test = features.loc[features['se_se2'] == 'se2']
     #Output csv files
-    trainVal.to_csv('OUTPUT\\TrainValidateData.csv')
-    test.to_csv('OUTPUT\\TestData.csv')
+    trainValfilepath = os.path.join(path, 'OUTPUT', 'TrainValidateData.csv')
+    testDatafilepath = os.path.join(path, 'OUTPUT', 'TestData.csv')
+    trainVal.to_csv(trainValfilepath)
+    test.to_csv(testDatafilepath)
+    
+    #Apply model and 3-tier testing scheme
+    modelResults = three_tier_test(features, chLabel)
+    #Get performance measures from model
+    plotPerformance(modelResults, chLabel)
 #             
 #%% SELF-RUN ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #Main Self-run block
